ad4fdcde16b52a7126474624018815ae53855638
Danik Samuta <116016852+Dezzzl@users.noreply.github.com>
2025-10-24 06:11:23 +0300

    task3: Третья лабораторная работа (#35)
    * task3: Третья лабораторная работа
    
    * task3: переместил .gitignore в папку samuta

---
+++ samuta/compiler/src/main/java/dev/dezzzl/GslErrorListener.java
@@ -0,0 +1,18 @@
+package dev.dezzzl;
+
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+
+public class GslErrorListener extends BaseErrorListener {
+
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer,
+                            Object offendingSymbol,
+                            int line,
+                            int charPositionInLine,
+                            String msg,
+                            RecognitionException e) {
+        System.err.printf("Error in the line %d:%d — %s%n", line, charPositionInLine, msg);
+    }
+}
