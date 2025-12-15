19159b9dd2c276633e51cb74a86f43cf11e53c61
alesh <aleshiaqwerty@gmail.com>
2025-12-15 18:04:38 +0300

    lab5

--- gordeyuk/compiler/constants.py
+++ gordeyuk/compiler/constants.py
@@ -6,7 +6,24 @@ TYPE_ARRAY = 'array'
 TYPE_INT = 'int'
 TYPE_BOOL = 'bool'
 TYPE_UNKNOWN = 'unknown'
+TYPE_VOID = "void"
 
 ARITHMETIC_OPS = ['+', '-', '*', '/']
 COMPARISON_OPS = ['==', '!=', '<', '>']
 LOGICAL_OPS = ['and', 'or', 'not']
+
+JVM_TYPE_DESCRIPTORS = {
+    TYPE_INT: 'I',
+    TYPE_CHAR: 'C',
+    TYPE_STRING: 'Ljava/lang/String;',
+    TYPE_ARRAY: '[Ljava/lang/String;',
+    TYPE_BOOL: 'Z',
+}
+
+JVM_TYPE_NAMES = {
+    TYPE_INT: 'int',
+    TYPE_CHAR: 'char',
+    TYPE_STRING: 'java/lang/String',
+    TYPE_ARRAY: '[Ljava/lang/String;',
+    TYPE_BOOL: 'boolean',
+}
